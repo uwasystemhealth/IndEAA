297523d89fc33b51e64f22ca224750aca99d9409
frinzekt <frinzekt@gmail.com>
2020-12-23 00:20:30 +0800

    make login page the landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,70 +1,79 @@
 import React from "react";
-// nodejs library that concatenates classes
-import classNames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
-
+import Email from "@material-ui/icons/Email";
+import People from "@material-ui/icons/People";
 // core components
 import Header from "components/MaterialKit/Header/Header.js";
+import HeaderLinks from "components/MaterialKit/Header/HeaderLinks.js";
 import Footer from "components/MaterialKit/Footer/Footer.js";
 import GridContainer from "components/MaterialKit/Grid/GridContainer.js";
 import GridItem from "components/MaterialKit/Grid/GridItem.js";
 import Button from "components/MaterialKit/CustomButtons/Button.js";
-import HeaderLinks from "components/MaterialKit/Header/HeaderLinks.js";
-import Parallax from "components/MaterialKit/Parallax/Parallax.js";
+import Card from "components/MaterialKit/Card/Card.js";
+import CardBody from "components/MaterialKit/Card/CardBody.js";
+import CardHeader from "components/MaterialKit/Card/CardHeader.js";
+import CardFooter from "components/MaterialKit/Card/CardFooter.js";
+import CustomInput from "components/MaterialKit/CustomInput/CustomInput.js";
 
-import styles from "assets/jss/nextjs-material-kit/pages/landingPage.js";
+import styles from "assets/jss/nextjs-material-kit/pages/loginPage.js";
 
-// Sections for this page
-import ProductSection from "pages-sections/LandingPage-Sections/ProductSection.js";
-import TeamSection from "pages-sections/LandingPage-Sections/TeamSection.js";
-import WorkSection from "pages-sections/LandingPage-Sections/WorkSection.js";
-
+import image from "assets/img/bg7.jpg";
 
 const useStyles = makeStyles(styles);
 
-export default function LandingPage(props) {
+export default function LoginPage(props) {
+  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+  setTimeout(function () {
+    setCardAnimation("");
+  }, 700);
   const classes = useStyles();
   return (
-    <>
-      <Parallax filter responsive image={require("assets/img/landing-bg.jpg")}>
-        <div className={classes.container}>
-          <GridContainer>
-            <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>Your Story Starts With Us.</h1>
-              <h4>
-                Every landing page needs a small description after the big bold
-                title, that{"'"}s why we added this text here. Add here all the
-                information that can make you or your product create the first
-                impression.
-              </h4>
-              <br />
-              <Button
-                color="danger"
-                size="lg"
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fas fa-play" />
-                Watch video
-              </Button>
-            </GridItem>
-          </GridContainer>
-        </div>
-      </Parallax>
-      <div className={classNames(classes.main, classes.mainRaised)}>
-        <div className={classes.container}>
-          <ProductSection />
-          <TeamSection />
-          <WorkSection />
-        </div>
+    <div
+      className={classes.pageHeader}
+      style={{
+        backgroundImage: "url(" + image + ")",
+        backgroundSize: "cover",
+        backgroundPosition: "top center"
+      }}
+    >
+      <div className={classes.container}>
+        <GridContainer justify="center">
+          <GridItem xs={12} sm={6} md={4}>
+            <Card className={classes[cardAnimaton]}>
+              <form className={classes.form}>
+                <CardHeader color="primary" className={classes.cardHeader}>
+                  <h4>Login</h4>
+                  <div className={classes.socialLine}>
+                    <Button
+                      justIcon
+                      href="#pablo"
+                      target="_blank"
+                      color="transparent"
+                      onClick={e => e.preventDefault()}
+                    >
+                      <i className={"fab fa-google-plus-g"} />
+                    </Button>
+                  </div>
+                </CardHeader>
+                {/* <CardBody>
+                  
+                  </CardBody> */}
+                <CardFooter className={classes.cardFooter}>
+                  <Button simple color="primary" size="lg">
+                    Get started
+                    </Button>
+                </CardFooter>
+              </form>
+            </Card>
+          </GridItem>
+        </GridContainer>
       </div>
-      <Footer />
-    </>
+    </div>
   );
 }
 
-LandingPage.customLayout = () => <></>
+LoginPage.customLayout=()=><></>
