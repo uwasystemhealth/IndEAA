1600cfbe1ec037002a12fa88bb0283664c3f5e8c
frinzekt <frinzekt@gmail.com>
2020-12-22 23:58:33 +0800

    starter pages

---
+++ client/pages/coordinator/[courseID]/documents.js
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router"
+
+//Styles
+import { makeStyles } from "@material-ui/core/styles";
+import styles from "assets/jss/nextjs-material-kit/pages/landingPage.js";
+const useStyles = makeStyles(styles);
+
+const CoordinatorCourseDocumentsPage = () => {
+    const router = useRouter()
+    const { courseID } = router.query
+
+    const classes = useStyles();
+    return (
+        <div className={classes.container}>
+            {courseID}
+            CoordinatorCourseDocumentsPage
+        </div>
+    )
+}
+
+export default CoordinatorCourseDocumentsPage
