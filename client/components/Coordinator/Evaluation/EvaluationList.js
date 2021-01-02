4e1e7036f68c9ce8f9ead91e8f1835e8ce89826e
Michael Nef <michael@nef.net.au>
2021-01-02 15:15:47 +0800

    CHANGED evaluation list to load only once

--- client/components/Coordinator/Evaluation/EvaluationList.js
+++ client/components/Coordinator/Evaluation/EvaluationList.js
@@ -2,30 +2,50 @@
 import Card from "components/MaterialKit/Card/Card.js";
 import CardBody from "components/MaterialKit/Card/CardBody.js";
 
+// CUSTOM COMPONENTS
+import EvaluationListing from "./EvaluationListing.js";
+
 //Styles
 import { makeStyles } from "@material-ui/core/styles";
 import styles from "assets/jss/nextjs-material-kit/pages/landingPage.js";
 const useStyles = makeStyles(styles);
 
-const CourseList = () => {
+import { useEffect, useState } from "react";
+
+const EvaluationList = () => {
   const classes = useStyles();
 
-  // 1. Find all CourseEvaluations where the createdBy key matches the logged in user
-  const CourseEvaluations = [
-    {
-      courseid: "MECH5521/MECH5522",
-      documents: [],
-      reviewDescription:
-        "This is a capstone unit for engineering students where they make something cool",
-      reviewTargetDate: "10/10/2021",
-      isArchived: false,
-      createdAt: "1/1/2021",
-      createdBy: {},
-      EOC: [],
-    },
-  ];
+  const [courseEvaluations, setCourseEvaluations] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // 1. Find all CourseEvaluations where the createdBy key matches the logged in user
+
+    setCourseEvaluations([
+      {
+        courseid: "MECH5521/MECH5522",
+        documents: [],
+        reviewDescription:
+          "This is a capstone unit for engineering students where they make something cool",
+        reviewTargetDate: "10/10/2021",
+        isArchived: false,
+        createdAt: "1/1/2021",
+        createdBy: {},
+        EOC: [],
+      },
+    ]);
+
+    setLoading(false);
+  }, []);
 
   // 2. Render course list elements
+  if (loading) {
+    return (
+      <Card>
+        <CardBody>Loading...</CardBody>
+      </Card>
+    );
+  }
 
   return (
     <Card>
@@ -34,4 +54,4 @@ const CourseList = () => {
   );
 };
 
-export default CourseList;
+export default EvaluationList;
