45939624f954a4f61a685ad2c0860bb6ceb57786
Michael Nef <michael@nef.net.au>
2021-01-02 16:24:42 +0800

    CHANGED styled evaluation listings using grids

--- client/components/Coordinator/Evaluation/EvaluationListing.js
+++ client/components/Coordinator/Evaluation/EvaluationListing.js
@@ -1,12 +1,23 @@
+// CORE
+import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf";
+import HowToVoteIcon from "@material-ui/icons/HowToVote";
+import Grid from "components/MaterialKit/Grid/GridContainer.js";
+import GridItem from "components/MaterialKit/Grid/GridItem.js";
+
 //Styles
 import { makeStyles } from "@material-ui/core/styles";
 import styles from "assets/jss/nextjs-material-kit/pages/landingPage.js";
-const useStyles = makeStyles({
-  ...styles,
-  listing: `
-display: grid;
-                              `,
-});
+const useStyles = makeStyles(() => ({
+  buttons: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  button: {
+    width: "5rem",
+    height: "5rem",
+  },
+}));
 
 const EvaluationListing = ({
   courseId,
@@ -16,13 +27,19 @@ const EvaluationListing = ({
   const classes = useStyles();
 
   return (
-    <div className={classes.listing}>
-      <h2>{courseId}</h2>
-      <br />
-      <h3>{coordinators}</h3>
-      <br />
-      <p>{evaluationDescription}</p>
-    </div>
+    <Grid>
+      <GridItem xs={9}>
+        <h2>{courseId}</h2>
+
+        <h3>{coordinators}</h3>
+
+        <p>{evaluationDescription}</p>
+      </GridItem>
+      <GridItem xs={3} className={classes.buttons}>
+        <PictureAsPdfIcon className={classes.button} />
+        <HowToVoteIcon className={classes.button} />
+      </GridItem>
+    </Grid>
   );
 };
 
