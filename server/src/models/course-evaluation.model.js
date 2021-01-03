f9ba568d2ee8d551dcdb9dbe6be2d58a26ada733
frinzekt <frinzekt@gmail.com>
2021-01-03 16:25:42 +0800

    courseEvaluation initial model

--- server/src/models/course-evaluation.model.js
+++ server/src/models/course-evaluation.model.js
@@ -1,16 +1,37 @@
 // courseEvaluation-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
+
+const DefaultSchema = require('../types/default.schema');
+const NameType = require('../types/name.type');
+const DescType = require('../types/desc.type');
+const LinkType = require("../types/link.type")
+
+const ObjectIdType = require('../types/objectId.type');
+
 module.exports = function (app) {
   const modelName = 'courseEvaluation';
   const mongooseClient = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
-  const schema = new Schema({
-    text: { type: String, required: true }
-  }, {
-    timestamps: true
-  });
+  const schema = DefaultSchema(app)
+
+  schema.add({
+    courseId: NameType(),
+    documents: [{
+      name: NameType(),
+      description: DescType(),
+      // TODO UPLOADED FILES - Stage 2
+      link: LinkType(),
+      tags: [{ type: "String" }]
+    }],
+    reviewDescription: DescType(),
+    isArchived: Boolean,
+    completedDate: Date,
+    eoc: [{
+      eocNumber: [String],
+      justification: DescType()
+    }]
+  })
 
   // This is necessary to avoid model compilation errors in watch mode
   // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
@@ -18,5 +39,5 @@ module.exports = function (app) {
     mongooseClient.deleteModel(modelName);
   }
   return mongooseClient.model(modelName, schema);
-  
+
 };
