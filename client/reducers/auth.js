b248eb4f4a78883460d2d8881926d753a583b2cd
frinzekt <frinzekt@gmail.com>
2021-01-03 12:35:16 +0800

    authentication reducer

---
+++ client/reducers/auth.js
@@ -0,0 +1,28 @@
+const initState = {
+    user: null,
+    error: null
+}
+
+// When populated, user     object will have the same fields as the mongoose type
+/*
+_id, createdAt, email, googleId, name, picture
+*/
+
+export const authenticateReducer = (state = initState, action) => {
+    switch (action.type) {
+        case "SIGNIN_SUCCESS":
+            return {
+                ...state,
+                user: action.user
+            }
+        case "SIGNIN_ERROR":
+            return {
+                ...state,
+                error: action.error
+            }
+        case "SIGNOUT_SUCCESS":
+            return state
+        default:
+            return state
+    }
+}
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,39 @@
 
 import { combineReducers } from 'redux';
 
+// Custom Reducers
+import { authenticateReducer } from "reducers/auth"
+
 export default function (reduxifiedServices) {
   return combineReducers({
     users: reduxifiedServices.users.reducer,
-    todo: reduxifiedServices.todo.reducer
+    todo: reduxifiedServices.todo.reducer,
+    auth: authenticateReducer
   });
 }
+
+/*
+To use (from https://redux.js.org/api/combinereducers)
+
+import { createStore } from 'redux'
+import reducer from './reducers/index'
+
+const store = createStore(reducer)
+console.log(store.getState())
+// {
+//   counter: 0,
+//   todos: []
+// }
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'Use Redux'
+})
+
+console.log(store.getState())
+// {
+//   counter: 0,
+//   todos: [ 'Use Redux' ]
+// }
+
+*/
