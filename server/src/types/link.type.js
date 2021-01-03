cdf66be551c7b88a521035167346be757b87a177
frinzekt <frinzekt@gmail.com>
2021-01-03 16:25:25 +0800

    link type

---
+++ server/src/types/link.type.js
@@ -0,0 +1,6 @@
+module.exports = (required=false, maxLengthSize=1024) => ({
+  type: String,
+  maxLength: [maxLengthSize, 'link is too long, try a url shortener.'],
+  match: [/https?:\/\/[\w-]+\.[\w-]+((\?|\/|#).*)?/, 'Invalid link provided.'],
+  required
+})
