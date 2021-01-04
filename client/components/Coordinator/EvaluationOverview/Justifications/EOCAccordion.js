1b7b58e30ccbc7c50f921ddb092b0f6f3f995a61
Michael Nef <michael@nef.net.au>
2021-01-04 20:06:02 +0800

    ADDED ECOCard component for coordinators

--- client/components/Coordinator/EvaluationOverview/Justifications/EOCAccordion.js
+++ client/components/Coordinator/EvaluationOverview/Justifications/EOCAccordion.js
@@ -7,6 +7,9 @@ import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
+// CUSTOM COMPONENTS
+import EOCCard from "./EOCCard.js";
+
 import { useState, useEffect } from "react";
 
 const EOCAccordion = () => {
@@ -23,31 +26,37 @@ const EOCAccordion = () => {
         setName: "Knowledge and Skill Base",
         EOCS: [
           {
+            _id: "aaaaa",
             EOCNum: 1,
             desc:
               "Comprehensive, theory based understanding of the underpinning natural and physical sciences and the engineering fundamentals applicable to the engineering discipline.",
           },
           {
+            _id: "aaaaa",
             EOCNum: 2,
             desc:
               "Conceptual understanding of the mathematics, numerical analysis, statistics, and computer and information sciences which underpin the engineering discipline",
           },
           {
+            _id: "aaaaa",
             EOCNum: 3,
             desc:
               "In-depth understanding of specialist bodies of knowledge within the engineering discipline.",
           },
           {
+            _id: "aaaaa",
             EOCNum: 4,
             desc:
               "Discernment of knowledge development and research directions within the engineering discipline.",
           },
           {
+            _id: "aaaaa",
             EOCNum: 5,
             desc:
               "Knowledge of engineering design practice and contextual factors impacting the engineering discipline.",
           },
           {
+            _id: "aaaaa",
             EOCNum: 6,
             desc:
               "Understanding of the scope, principles, norms, accountabilities and bounds of sustainable engineering practice in the specific discipline.",
@@ -60,21 +69,25 @@ const EOCAccordion = () => {
         setName: "Engineering Application Ability",
         EOCS: [
           {
+            _id: "aaaaa",
             EOCNum: 1,
             desc:
               "Application of established engineering methods to complex engineering problem solving.",
           },
           {
+            _id: "aaaaa",
             EOCNum: 2,
             desc:
               "Fluent application of engineering techniques, tools and resources.",
           },
           {
+            _id: "aaaaa",
             EOCNum: 3,
             desc:
               "Application of systematic engineering synthesis and design processes.",
           },
           {
+            _id: "aaaaa",
             EOCNum: 4,
             desc:
               "Application of systematic approaches to the conduct and management of engineering projects.",
@@ -87,27 +100,33 @@ const EOCAccordion = () => {
         setName: "Professional and Personal Attributes",
         EOCS: [
           {
+            _id: "aaaaa",
             EOCNum: 1,
             desc: "Ethical conduct and professional accountability",
           },
           {
+            _id: "aaaaa",
             EOCNum: 2,
             desc:
               "Effective oral and written communication in professional and lay domains",
           },
           {
+            _id: "aaaaa",
             EOCNum: 3,
             desc: "Creative, innovative and pro-active demeanour",
           },
           {
+            _id: "aaaaa",
             EOCNum: 4,
             desc: "Professional use and management of information",
           },
           {
+            _id: "aaaaa",
             EOCNum: 5,
             desc: "Orderly management of self, and professional conduct",
           },
           {
+            _id: "aaaaa",
             EOCNum: 6,
             desc: "Effective team membership and team leadership",
           },
---
+++ client/components/Coordinator/EvaluationOverview/Justifications/EOCCard.js
@@ -0,0 +1,5 @@
+const EOCCard = () => {
+  return <div></div>;
+};
+
+export default EOCCard;
