538fe44e8466e102127b9519f20236b5443f88b2
Michael Nef <michael@nef.net.au>
2021-01-04 20:37:27 +0800

    ADDED Button to view EOC coordinator modal

--- client/components/Coordinator/EvaluationOverview/Justifications/EOCCard.js
+++ client/components/Coordinator/EvaluationOverview/Justifications/EOCCard.js
@@ -8,6 +8,8 @@ import Success from "components/MaterialKit/Typography/Success.js";
 import Danger from "components/MaterialKit/Typography/Danger.js";
 import GridContainer from "components/MaterialKit/Grid/GridContainer.js";
 import GridItem from "components/MaterialKit/Grid/GridItem.js";
+import Button from "components/MaterialKit/CustomButtons/Button.js";
+import FindInPageIcon from "@material-ui/icons/FindInPage";
 
 const EOCCard = ({ title, description, rating, justification, eocID }) => {
   const ratingMsg =
@@ -28,7 +30,17 @@ const EOCCard = ({ title, description, rating, justification, eocID }) => {
     <Card>
       <CardHeader>{title}</CardHeader>
       <CardBody>
-        <Muted>{description}</Muted>
+        <GridContainer>
+          <GridItem xs={6}>
+            <Muted>{description}</Muted>
+          </GridItem>
+          <GridItem xs={6}>
+            <Button>
+              <FindInPageIcon />
+              View
+            </Button>
+          </GridItem>
+        </GridContainer>
       </CardBody>
       <CardFooter>
         <GridContainer direction="column" alignItems="flex-start">
