b4953ee9f5bca3914b576b1d6fd2651fca9f4b3c
Michael Nef <michael@nef.net.au>
2021-01-04 18:27:10 +0800

    ADDED navigation tabs for evaluation page

--- client/components/Coordinator/EvaluationOverview/GeneralPage.js
+++ client/components/Coordinator/EvaluationOverview/GeneralPage.js
@@ -1,20 +1,76 @@
 // CORE COMPONENTS
-import Card from "components/MaterialKit/Card/Card.js";
-import CardBody from "components/MaterialKit/Card/CardBody.js";
-import CardHeader from "components/MaterialKit/Card/CardHeader.js";
+import CustomTabs from "components/MaterialKit/CustomTabs/CustomTabs.js";
+import Face from "@material-ui/icons/Face";
+import Chat from "@material-ui/icons/Chat";
+import Build from "@material-ui/icons/Build";
 
 // Styles
 import styles from "assets/jss/nextjs-material-kit/pages/landingPage.js";
 import { makeStyles } from "@material-ui/core/styles";
-const useStyles = makeStyles(styles);
+const customStyles = {
+  textCenter: {
+    textAlign: "center",
+  },
+};
+const useStyles = makeStyles({ ...styles, ...customStyles });
 
 const GeneralPage = ({ courseID }) => {
   const classes = useStyles();
   return (
-    <Card>
-      <CardHeader></CardHeader>
-      <CardBody></CardBody>
-    </Card>
+    <CustomTabs
+      headerColor="success"
+      tabs={[
+        {
+          tabName: "General",
+          tabIcon: Face,
+          tabContent: (
+            <p className={classes.textCenter}>
+              I think that’s a responsibility that I have, to push
+              possibilities, to show people, this is the level that things could
+              be at. So when you get something that has the name Kanye West on
+              it, it’s supposed to be pushing the furthest possibilities. I will
+              be the leader of a company that ends up being worth billions of
+              dollars, because I got the answers. I understand culture. I am the
+              nucleus.
+            </p>
+          ),
+        },
+        {
+          tabName: "Justifications",
+          tabIcon: Chat,
+          tabContent: (
+            <p className={classes.textCenter}>
+              I think that’s a responsibility that I have, to push
+              possibilities, to show people, this is the level that things could
+              be at. I will be the leader of a company that ends up being worth
+              billions of dollars, because I got the answers. I understand
+              culture. I am the nucleus. I think that’s a responsibility that I
+              have, to push possibilities, to show people, this is the level
+              that things could be at.
+            </p>
+          ),
+        },
+        {
+          tabName: "Documents",
+          tabIcon: Chat,
+          tabContent: (
+            <p className={classes.textCenter}>
+              think that’s a responsibility that I have, to push possibilities,
+              to show people, this is the level that things could be at. So when
+              you get something that has the name Kanye West on it, it’s
+              supposed to be pushing the furthest possibilities. I will be the
+              leader of a company that ends up being worth billions of dollars,
+              because I got the answers. I understand culture. I am the nucleus.
+            </p>
+          ),
+        },
+        {
+          tabName: "Reviews",
+          tabIcon: Build,
+          tabContent: <p className={classes.textCenter}> yeeettt</p>,
+        },
+      ]}
+    />
   );
 };
 
