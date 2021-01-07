f09605af96305eca242e1e78c25b8af75bcba7b5
Michael Nef <michael@nef.net.au>
2021-01-07 21:06:30 +0800

    ADDED Content to EOC modal popups

--- client/components/Coordinator/EvaluationOverview/Justifications/ManageEOC.js
+++ client/components/Coordinator/EvaluationOverview/Justifications/ManageEOC.js
@@ -5,11 +5,29 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
+import GridContainer from "components/MaterialKit/Grid/GridContainer.js";
+import GridItem from "components/MaterialKit/Grid/GridItem.js";
+import CustomDropdown from "components/MaterialKit/CustomDropdown/CustomDropdown.js";
+import TextField from "@material-ui/core/TextField";
+import Card from "components/MaterialKit/Card/Card.js";
+import CardBody from "components/MaterialKit/Card/CardBody.js";
+import CardHeader from "components/MaterialKit/Card/CardHeader.js";
+import HelpIcon from "@material-ui/icons/Help";
+
+// CUSTOM COMPONENTS
+import ApplyTo from "./ApplyTo.js";
+
+// STYLES
+import { makeStyles } from "@material-ui/core/styles";
+const styles = {};
+const useStyles = makeStyles(styles);
 
 import React, { useState } from "react";
 
-const ManageEOC = ({ title }) => {
+const ManageEOC = ({ title, description }) => {
+  const classes = useStyles();
   const [modal, setModal] = useState(false);
+  const [developmentLevel, setDevelopmentLevel] = useState("None");
 
   return (
     <>
@@ -24,7 +42,39 @@ const ManageEOC = ({ title }) => {
         maxWidth="lg"
         fullWidth
       >
-        <DialogTitle>{title}</DialogTitle>
+        <DialogTitle>
+          {title} - {description}
+        </DialogTitle>
+
+        <DialogContent>
+          <GridContainer>
+            <GridItem xs={6}>
+              Development Level
+              <HelpIcon />
+              <CustomDropdown
+                buttonText={developmentLevel}
+                dropdownList={[
+                  "Level 1 - Foundational",
+                  "Level 2 - Broad and Coherent",
+                  "Level 3 - Advanced",
+                  "Level 4 - Specialist",
+                ]}
+              />
+            </GridItem>
+            <GridItem xs={6}>
+              <ApplyTo />
+            </GridItem>
+            <GridItem xs={6}>
+              <Card>
+                <CardHeader color="success">Justification</CardHeader>
+                <CardBody>
+                  <TextField multiline fullWidth />
+                </CardBody>
+              </Card>
+            </GridItem>
+            <GridItem xs={6}>general documents</GridItem>
+          </GridContainer>
+        </DialogContent>
       </Dialog>
     </>
   );
