f1139a9b93b5164242b733edfbede9c49082dcd9
Michael Nef <michael@nef.net.au>
2021-01-07 14:00:52 +0800

    CHANGE styles on reviews page to look organised

--- client/components/Coordinator/EvaluationOverview/Reviews/index.js
+++ client/components/Coordinator/EvaluationOverview/Reviews/index.js
@@ -1,13 +1,21 @@
+// CORE COMPONENTS
+import GridContainer from "components/MaterialKit/Grid/GridContainer.js";
+import GridItem from "components/MaterialKit/Grid/GridItem.js";
+
 // CUSTOM COMPONENTS
 import ReviewProgress from "./ReviewProgress";
 import OtherFunctions from "./OtherFunctions";
 
 const Reviews = () => {
   return (
-    <div>
-      <ReviewProgress />
-      <OtherFunctions />
-    </div>
+    <GridContainer>
+      <GridItem xs={7}>
+        <ReviewProgress />
+      </GridItem>
+      <GridItem xs={5}>
+        <OtherFunctions />
+      </GridItem>
+    </GridContainer>
   );
 };
 
