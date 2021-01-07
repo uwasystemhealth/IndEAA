f0928c663bfdca9fdca6ded38ed6bb6fcad2fd0f
Michael Nef <michael@nef.net.au>
2021-01-07 13:51:07 +0800

    CHANGE data processing on reviewProgress list

--- client/components/Coordinator/EvaluationOverview/Reviews/ReviewProgress/index.js
+++ client/components/Coordinator/EvaluationOverview/Reviews/ReviewProgress/index.js
@@ -2,28 +2,60 @@
 import Card from "components/MaterialKit/Card/Card.js";
 import CardBody from "components/MaterialKit/Card/CardBody.js";
 import CardHeader from "components/MaterialKit/Card/CardHeader.js";
+import GridContainer from "components/MaterialKit/Grid/GridContainer.js";
+import GridItem from "components/MaterialKit/Grid/GridItem.js";
 
 // CUSTOM COMPONENTS
 import ProgressDisplay from "./ProgressDisplay.js";
 
+import { useState, useEffect } from "react";
+
 const ReviewProgress = () => {
-  const reviewers = [
-    {
-      name: "Michael Nefiodovas",
-      email: "michael.nefiodovas@uwa.edu.au",
-      stage: 0,
-      reviewID: "asdsafdg",
-    },
-  ];
+  const [reviewers, setReviewers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const revs = [
+      {
+        name: "Michael Nefiodovas",
+        email: "michael.nefiodovas@uwa.edu.au",
+        stage: 0,
+        reviewID: "asdsafdg",
+      },
+      {
+        name: "Frinze Lapuz",
+        email: "frinze.lapuz@uwa.edu.au",
+        stage: 3,
+        reviewID: "yeet",
+      },
+      {
+        name: "Melinda Hodkiewicz",
+        email: "melinda.hodkiewicz@uwa.edu.au",
+        stage: 4,
+        reviewID: "helloworld",
+      },
+    ];
+
+    setReviewers(revs);
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <p>loading...</p>;
+  }
 
   const progressCards = reviewers.map((reviewer) => (
-    <ProgressDisplay key={reviewer.reviewID} {...reviewer}></ProgressDisplay>
+    <GridItem key={reviewer.reviewID}>
+      <ProgressDisplay {...reviewer}></ProgressDisplay>
+    </GridItem>
   ));
 
   return (
     <Card>
       <CardHeader color="success">Review Progress</CardHeader>
-      <CardBody>{progressCards}</CardBody>
+      <CardBody>
+        <GridContainer>{progressCards}</GridContainer>
+      </CardBody>
     </Card>
   );
 };
