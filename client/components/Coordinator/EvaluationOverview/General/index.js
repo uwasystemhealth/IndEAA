f6def3408792e82d52351fe2917adc70e56b63ba
Michael Nef <michael@nef.net.au>
2021-01-07 14:16:06 +0800

    Organised coordinator general page

--- client/components/Coordinator/EvaluationOverview/General/index.js
+++ client/components/Coordinator/EvaluationOverview/General/index.js
@@ -2,14 +2,20 @@
 import Information from "./Information.js";
 import OtherInformation from "./OtherInformation.js";
 import Controls from "./Controls.js";
+import GridContainer from "components/MaterialKit/Grid/GridContainer.js";
+import GridItem from "components/MaterialKit/Grid/GridItem.js";
 
 const General = () => {
   return (
-    <div>
-      <Information />
-      <OtherInformation />
-      <Controls />
-    </div>
+    <GridContainer>
+      <GridItem xs={6}>
+        <Information />
+      </GridItem>
+      <GridItem xs={6}>
+        <OtherInformation />
+        <Controls />
+      </GridItem>
+    </GridContainer>
   );
 };
 
