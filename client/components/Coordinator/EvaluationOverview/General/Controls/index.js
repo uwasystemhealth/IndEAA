cf69ccee2c3fb4987155654056cafc66f020d8f2
Michael Nef <michael@nef.net.au>
2021-01-07 18:11:48 +0800

    CHANGE rename course -> review (consistency reasons)

--- client/components/Coordinator/EvaluationOverview/General/Controls/index.js
+++ client/components/Coordinator/EvaluationOverview/General/Controls/index.js
@@ -2,7 +2,7 @@ import Card from "components/MaterialKit/Card/Card.js";
 import CardBody from "components/MaterialKit/Card/CardBody.js";
 import CardHeader from "components/MaterialKit/Card/CardHeader.js";
 import Button from "components/MaterialKit/CustomButtons/Button.js";
-import ManageCoordinators from "./ManageCoordinators.js";
+import ManageReviewers from "./ManageReviewers.js";
 
 import { useState } from "react";
 
@@ -12,7 +12,7 @@ const Controls = () => {
       <CardBody>
         <Button color="warning">Archive</Button>
         <Button color="warning">Force Completion</Button>
-        <ManageCoordinators />
+        <ManageReviewers />
       </CardBody>
     </Card>
   );
