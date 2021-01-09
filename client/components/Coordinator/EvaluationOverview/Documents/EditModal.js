8add167b0da2467efe30b6684072f5ba985f6d5b
Michael Nef <michael@nef.net.au>
2021-01-09 21:24:09 +0800

    ADDED Document metatdata inputs on "add new document"

--- client/components/Coordinator/EvaluationOverview/Documents/EditModal.js
+++ client/components/Coordinator/EvaluationOverview/Documents/EditModal.js
@@ -5,8 +5,13 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import GridContainer from "components/MaterialKit/Grid/GridContainer.js";
 import GridItem from "components/MaterialKit/Grid/GridItem.js";
-
+import CustomInput from "components/MaterialKit/CustomInput/CustomInput.js";
+import TextField from "@material-ui/core/TextField";
 import EditIcon from "@material-ui/icons/Edit";
+import SubjectIcon from "@material-ui/icons/Subject";
+import Card from "components/MaterialKit/Card/Card.js";
+import CardBody from "components/MaterialKit/Card/CardBody.js";
+import CardHeader from "components/MaterialKit/Card/CardHeader.js";
 
 // CUSTOM COMPONENTS
 import Tags from "./Tags.js";
@@ -31,8 +36,44 @@ const EditModal = () => {
         <DialogTitle>Add New Document</DialogTitle>
         <DialogContent>
           <GridContainer>
-            <GridItem xs={6}>Name, desc link</GridItem>
-            <GridItem xs={6}>
+            <GridItem xs={5}>
+              <Card>
+                <CardHeader color="success">Document Metadata</CardHeader>
+                <CardBody>
+                  <GridContainer justify="center">
+                    <GridItem xs={12}>
+                      <CustomInput
+                        labelText="Name"
+                        formControlProps={{
+                          fullWidth: true,
+                        }}
+                      />
+                    </GridItem>
+                    <GridItem xs={12}>
+                      <CustomInput
+                        labelText="Description"
+                        inputProps={{
+                          multiline: true,
+                          rows: 4,
+                        }}
+                        formControlProps={{
+                          fullWidth: true,
+                        }}
+                      />
+                    </GridItem>
+                    <GridItem xs={12}>
+                      <CustomInput
+                        labelText="Link"
+                        formControlProps={{
+                          fullWidth: true,
+                        }}
+                      />
+                    </GridItem>
+                  </GridContainer>
+                </CardBody>
+              </Card>
+            </GridItem>
+            <GridItem xs={7}>
               <Tags />
             </GridItem>
           </GridContainer>
