c7a01d9c4a8134ed8bd80890989f6e65170a6566
Michael Nef <michael@nef.net.au>
2021-01-09 22:41:14 +0800

    ADDED document loading for specific & documents in justification section

--- client/components/Coordinator/EvaluationOverview/Justifications/DocumentViewer.js
+++ client/components/Coordinator/EvaluationOverview/Justifications/DocumentViewer.js
@@ -7,25 +7,71 @@ import Face from "@material-ui/icons/Face";
 import Chat from "@material-ui/icons/Chat";
 
 // CUSTOM COMPONENTS
+import DocumentCard from "./../Documents/DocumentCard.js";
 
 const DocumentViewer = ({ documents }) => {
-  return (
-    <CustomTabs
-      headerColor="success"
-      tabs={[
+    const generalDocs = [
         {
-          tabName: "General Documents",
-          tabIcon: Face,
-          tabContent: <p>General documents go here</p>,
+            documentID: "asdfasd",
+            title: "Design Project Outline",
+            createdDate: new Date("2019-12-19"),
+            uri:
+                "https://docs.google.com/document/d/1rdLcaVBP_z-vE_5-gbaOevRbd6e_vkRjTQpuXyRF_FU/",
+            eocs: [1, 2, 3],
         },
         {
-          tabName: "Specific Documents",
-          tabIcon: Chat,
-          tabContent: <p>Content for specific EOCs goes here</p>,
+            documentID: "asdfasd2",
+            title: "RTIO Project - J Slack Something blah blah",
+            createdDate: new Date("2019-12-19"),
+            uri:
+                "https://docs.google.com/document/d/1rdLcaVBP_z-vE_5-gbaOevRbd6e_vkRjTQpuXyRF_FU/",
+            eocs: [1.1],
         },
-      ]}
-    />
-  );
+    ];
+
+    const specificDocs = [
+        {
+            documentID: "asdfasd",
+            title: "specific Design Project Outline",
+            createdDate: new Date("2019-12-19"),
+            uri:
+                "https://docs.google.com/document/d/1rdLcaVBP_z-vE_5-gbaOevRbd6e_vkRjTQpuXyRF_FU/",
+            eocs: [1, 2, 3],
+        },
+        {
+            _id: "asdfasd2",
+            title: "specific !! fdsafRTIO Project - J Slack Something blah blah",
+            createdDate: new Date("2019-12-19"),
+            uri:
+                "https://docs.google.com/document/d/1rdLcaVBP_z-vE_5-gbaOevRbd6e_vkRjTQpuXyRF_FU/",
+            eocs: [1.1],
+        },
+    ];
+
+    const generalCards = generalDocs.map((doc) => (
+        <DocumentCard key={doc._id} {...doc} />
+    ));
+    const specificCards = specificDocs.map((doc) => (
+        <DocumentCard key={doc._id} {...doc} />
+    ));
+
+    return (
+        <CustomTabs
+            headerColor="success"
+            tabs={[
+                {
+                    tabName: "General Documents",
+                    tabIcon: Face,
+                    tabContent: <>{generalCards}</>,
+                },
+                {
+                    tabName: "Specific Documents",
+                    tabIcon: Chat,
+                    tabContent: <>{specificCards}</>,
+                },
+            ]}
+        />
+    );
 };
 
 export default DocumentViewer;
