80210f7489372ff91d294cb2759ca1d76a32ddd6
Michael Nef <michael@nef.net.au>
2021-01-10 20:24:54 +0800

    CHANGE prettified and added dueDate field

--- server/support/insertSampleCourses.js
+++ server/support/insertSampleCourses.js
@@ -3,59 +3,83 @@ This is a support file that is used to insert sample courses to the database
 This is comparable to Djang Management Commands Scripts
 */
 
-const app = require('../src/app');
+const app = require("../src/app");
 
 const insertSampleCourses = async () => {
-    console.log("Starting: insertSampleCourses")
-    data = [
+  console.log("Starting: insertSampleCourses");
+  data = [
+    {
+      courseId: "CITS1001",
+      documents: [
         {
-            courseId: "CITS1001",
-            documents: [
-                { name: "Sample Document", link: "https://www.google.com", tags: ["1", "1.1"] }
-            ],
-            reviewDescription: "Sample Description",
-            reviewTargetDate: "10/10/2021",
-            isArchived: false,
-            createdAt: "1/1/2021",
-            createdBy: {},
-            eoc: [
-                { eocNumber: ["1.1", "1.2", "1.3"], justification: "Sample Justification" }
-            ],
+          name: "Sample Document",
+          link: "https://www.google.com",
+          tags: ["1", "1.1"],
         },
+      ],
+      reviewDescription: "Sample Description",
+      reviewTargetDate: "10/10/2021",
+      isArchived: false,
+      dueDate: "3/3/2021",
+      createdAt: "1/1/2021",
+      createdBy: {},
+      eoc: [
         {
-            courseId: "MECH1001",
-            documents: [
-                { name: "Sample Document", link: "https://www.google.com", tags: ["1", "1.1"] }
-            ],
-            reviewDescription: "Sample Description",
-            reviewTargetDate: "10/10/2021",
-            isArchived: false,
-            createdAt: "1/1/2021",
-            createdBy: {},
-            eoc: [
-                { eocNumber: ["1.1", "1.2", "1.3"], justification: "Sample Justification" }
-            ],
+          eocNumber: ["1.1", "1.2", "1.3"],
+          justification: "Sample Justification",
         },
+      ],
+    },
+    {
+      courseId: "MECH1001",
+      documents: [
         {
-            courseId: "ELEC1001",
-            documents: [
-                { name: "Sample Document", link: "https://www.google.com", tags: ["1", "1.1"] }
-            ],
-            reviewDescription: "Sample Description",
-            reviewTargetDate: "10/10/2021",
-            isArchived: false,
-            createdAt: "1/1/2021",
-            createdBy: {},
-            eoc: [
-                { eocNumber: ["1.1", "1.2", "1.3"], justification: "Sample Justification" }
-            ],
-        }
-    ]
-    const promises = data.map(datum => {
-        return app.service("course-evaluation").create(datum)
-    })
-    await Promise.all(promises)
-    console.log("Done")
-}
+          name: "Sample Document",
+          link: "https://www.google.com",
+          tags: ["1", "1.1"],
+        },
+      ],
+      reviewDescription: "Sample Description",
+      reviewTargetDate: "10/10/2021",
+      isArchived: false,
+      createdAt: "1/1/2021",
+      dueDate: "3/3/2021",
+      createdBy: {},
+      eoc: [
+        {
+          eocNumber: ["1.1", "1.2", "1.3"],
+          justification: "Sample Justification",
+        },
+      ],
+    },
+    {
+      courseId: "ELEC1001",
+      documents: [
+        {
+          name: "Sample Document",
+          link: "https://www.google.com",
+          tags: ["1", "1.1"],
+        },
+      ],
+      reviewDescription: "Sample Description",
+      reviewTargetDate: "10/10/2021",
+      isArchived: false,
+      dueDate: "3/3/2021",
+      createdAt: "1/1/2021",
+      createdBy: {},
+      eoc: [
+        {
+          eocNumber: ["1.1", "1.2", "1.3"],
+          justification: "Sample Justification",
+        },
+      ],
+    },
+  ];
+  const promises = data.map((datum) => {
+    return app.service("course-evaluation").create(datum);
+  });
+  await Promise.all(promises);
+  console.log("Done");
+};
 
-insertSampleCourses()
+insertSampleCourses();
