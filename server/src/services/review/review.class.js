368e7e51c523013adc319a8d26e441bfc0188fa2
frinzekt <frinzekt@gmail.com>
2021-01-16 18:23:06 +0800

    review service (cherry picked)

---
+++ server/src/services/review/review.class.js
@@ -0,0 +1,5 @@
+const { Service } = require('feathers-mongoose');
+
+exports.Review = class Review extends Service {
+  
+};
---
+++ server/test/services/review.test.js
@@ -0,0 +1,8 @@
+const app = require('../../src/app');
+
+describe('\'review\' service', () => {
+  it('registered the service', () => {
+    const service = app.service('review');
+    expect(service).toBeTruthy();
+  });
+});
