54a9496ccd9839e651cd6c13f0740621626d092c
frinzekt <frinzekt@gmail.com>
2021-01-20 01:28:37 +0800

    eoc review accordion display

--- client/pages/reviewer/[courseID]/1-overview-and-eoc.js
+++ client/pages/reviewer/[courseID]/1-overview-and-eoc.js
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 // CORE COMPONENTS
-import Button from "components/MaterialKit/CustomButtons/Button.js";
-import CardFooter from "components/MaterialKit/Card/CardFooter.js";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import GridContainer from "components/MaterialKit/Grid/GridContainer.js";
 import GridItem from "components/MaterialKit/Grid/GridItem.js";
+import CustomTabs from "components/MaterialKit/CustomTabs/CustomTabs.js";
 
 // Use own components
 import ReviewProgress from "components/reviewer/ReviewProgress";
@@ -17,7 +20,7 @@ import { services } from "store/feathersClient";
 
 //Styles
 import { makeStyles } from "@material-ui/core/styles";
-import styles from "assets/jss/nextjs-material-kit/pages/landingPage.js";
+import styles from "assets/jss/nextjs-material-kit/pages/loginPage.js";
 const useStyles = makeStyles(styles);
 
 import { getOrCreateReview } from "utils";
@@ -44,12 +47,165 @@ const ReviewerCourseReviewPage1 = () => {
   return (
     <div>
       <ReviewProgress review={review}></ReviewProgress>
+      <EOCDescriptionAccordions></EOCDescriptionAccordions>
       <ReviewerPageBottomNavigation
         pageNumber={1}
         course_id={courseID}
       ></ReviewerPageBottomNavigation>
     </div>
   );
+};
+
+const EOCDescriptionAccordions = () => {
+  const [eocs, setEocs] = useState(() => getEOCInfo());
+
+  // This is a copy of the coordinator/EOCAccordion (modified)
+  // This needs to be refactored later on
+  // Ideally the aggreed upon
+
+  const classes = useStyles();
+
+  return (
+    <>
+      {eocs.map((eocSet) => (
+        <Accordion key={eocSet.setNum}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            EOC {eocSet.setNum}: {eocSet.setName}
+          </AccordionSummary>
+          <AccordionDetails>
+            <CustomTabs
+              headerColor="primary"
+              tabs={eocSet.EOCS.map(eoc=>({
+                  tabName: `EOC ${eocSet.setNum}.${eoc.EOCNum}`,
+                  tabContent: <div>
+                      <h4 className={classes.title}>{eocSet.setName}</h4>
+                      {eoc.desc}
+                  </div>
+              }))}
+            />
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </>
+  );
+};
+
+// TODO Remove this once obtained the EOC info
+const getEOCInfo = (evaluationID) => {
+  return [
+    {
+      _id: "1",
+      setNum: 1,
+      setName: "Knowledge and Skill Base",
+      EOCS: [
+        {
+          _id: "2",
+          EOCNum: 1,
+          desc:
+            "Comprehensive, theory based understanding of the underpinning natural and physical sciences and the engineering fundamentals applicable to the engineering discipline.",
+        },
+        {
+          _id: "3",
+          EOCNum: 2,
+          desc:
+            "Conceptual understanding of the mathematics, numerical analysis, statistics, and computer and information sciences which underpin the engineering discipline",
+        },
+        {
+          _id: "4",
+          EOCNum: 3,
+          desc:
+            "In-depth understanding of specialist bodies of knowledge within the engineering discipline.",
+        },
+        {
+          _id: "5",
+          EOCNum: 4,
+          desc:
+            "Discernment of knowledge development and research directions within the engineering discipline.",
+        },
+        {
+          _id: "6",
+          EOCNum: 5,
+          desc:
+            "Knowledge of engineering design practice and contextual factors impacting the engineering discipline.",
+        },
+        {
+          _id: "7",
+          EOCNum: 6,
+          desc:
+            "Understanding of the scope, principles, norms, accountabilities and bounds of sustainable engineering practice in the specific discipline.",
+        },
+      ],
+    },
+    {
+      _id: "8",
+      setNum: 2,
+      setName: "Engineering Application Ability",
+      EOCS: [
+        {
+          _id: "9",
+          EOCNum: 1,
+          desc:
+            "Application of established engineering methods to complex engineering problem solving.",
+        },
+        {
+          _id: "10",
+          EOCNum: 2,
+          desc:
+            "Fluent application of engineering techniques, tools and resources.",
+        },
+        {
+          _id: "11",
+          EOCNum: 3,
+          desc:
+            "Application of systematic engineering synthesis and design processes.",
+        },
+        {
+          _id: "12",
+          EOCNum: 4,
+          desc:
+            "Application of systematic approaches to the conduct and management of engineering projects.",
+        },
+      ],
+    },
+    {
+      _id: "13",
+      setNum: 3,
+      setName: "Professional and Personal Attributes",
+      EOCS: [
+        {
+          _id: "14",
+          EOCNum: 1,
+          desc: "Ethical conduct and professional accountability",
+        },
+        {
+          _id: "15",
+          EOCNum: 2,
+          desc:
+            "Effective oral and written communication in professional and lay domains",
+        },
+        {
+          _id: "16",
+          EOCNum: 3,
+          desc: "Creative, innovative and pro-active demeanour",
+        },
+        {
+          _id: "17",
+          EOCNum: 4,
+          desc: "Professional use and management of information",
+        },
+        {
+          _id: "18",
+          EOCNum: 5,
+          desc: "Orderly management of self, and professional conduct",
+        },
+        {
+          _id: "19",
+          EOCNum: 6,
+          desc: "Effective team membership and team leadership",
+        },
+      ],
+    },
+  ];
 };
 
 export default ReviewerCourseReviewPage1;
