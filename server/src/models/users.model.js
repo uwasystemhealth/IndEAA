93cadbcd5379423a4d1b589c9270c0d1f98e1b0e
Michael Nef <michael@nef.net.au>
2021-01-25 10:24:02 +0800

    CHANGE: users -> user fixes mongoose rename

--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -2,28 +2,31 @@
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-const DefaultSchema = require('../types/default.schema');
-const NameType = require('../types/name.type');
-const EmailType = require('../types/email.type');
-const ObjectIdType = require('../types/objectId.type');
+const DefaultSchema = require("../types/default.schema");
+const NameType = require("../types/name.type");
+const EmailType = require("../types/email.type");
+const ObjectIdType = require("../types/objectId.type");
 
-
-module.exports = function (app) {
-    const modelName = 'users';
-    const mongooseClient = app.get('mongooseClient');
+module.exports = function(app) {
+    const modelName = "user";
+    const mongooseClient = app.get("mongooseClient");
     const schema = DefaultSchema(app);
 
     schema.add({
         googleId: { type: String },
-        name: NameType(required = false),
+        name: NameType((required = false)),
         picture: { type: String },
         email: EmailType({ unique: true }),
         perms: [
             {
-                course_id: ObjectIdType('course-evaluation', app, required = false),
-                role: { type: String, enum: ['Administrator', 'Coordinator', 'Reviewer'], required: true },
-            }
-        ]
+                course_id: ObjectIdType("course-evaluation", app, (required = false)),
+                role: {
+                    type: String,
+                    enum: ["Administrator", "Coordinator", "Reviewer"],
+                    required: true,
+                },
+            },
+        ],
     });
 
     // This is necessary to avoid model compilation errors in watch mode
@@ -32,5 +35,4 @@ module.exports = function (app) {
         mongooseClient.deleteModel(modelName);
     }
     return mongooseClient.model(modelName, schema);
-
 };
