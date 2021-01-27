bfb3c1f42122b37b68b938aaf4b38e6bcae38ce1
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-01-28 02:18:22 +0800

    initial work on the compiled page

---
+++ client/pages/coordinator/[courseID]/review/compiled.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+// Redux
+import { useSelector } from 'react-redux';
+import { services } from 'store/feathersClient';
+
+// Custom Components
+import ReviewDocumentsComments from 'components/Coordinator/ReviewCompiled/ReviewDocumentsComment.js';
+import ReviewGeneralCommentCards from 'components/Coordinator/ReviewCompiled/ReviewGeneralCommentCards';
+import ReviewEOCTable from 'components/Coordinator/ReviewCompiled/ReviewEOCTable';
+
+
+import {linkReviewerAndReview} from 'utils/compileResult';
+
+const CompiledPage = () => {
+    const router = useRouter();
+    const {courseID} = router.query;
+
+    useEffect(() => {
+        services['course-evaluation'].get(courseID); // Accessed by data
+        services['review'].find({
+            query:{
+                course_id: courseID
+            },
+        }); //Accessed by queryResult.data
+        services['users'].find(
+            {query:
+               { 
+                   perms: {
+                       $elemMatch: { course_id: courseID, role: 'Reviewer' },
+                   }
+               }
+            }
+        ); // Accessed by queryResult.data
+    }, [courseID]);
+
+    const userState = useSelector(state=> state.users);
+    const reviewers = userState?.queryResult.data;
+    const reviewState = useSelector(state=> state.review);
+    const reviews = reviewState?.queryResult.data;
+
+    // Get only the linked if both are defined otherwise and empty array is used
+    const reviewsUserLinked = (reviewers && reviews && linkReviewerAndReview(reviewers,reviews)) || [];
+    return (
+        <div>
+            <ReviewDocumentsComments reviewsUserLinked={reviewsUserLinked}/>
+            <ReviewGeneralCommentCards reviewsUserLinked={reviewsUserLinked}/>
+            <ReviewEOCTable reviewsUserLinked={reviewsUserLinked} />
+        </div>
+    );
+};
+
+export default CompiledPage;
