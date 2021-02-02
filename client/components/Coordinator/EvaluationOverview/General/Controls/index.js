fe581aafe81f82b3b8e85bb1af6dc9a9dff21698
Michael Nef <michael@nef.net.au>
2021-02-02 15:43:24 +0800

    ADDED: Edit Evaluation button

--- client/components/Coordinator/EvaluationOverview/General/Controls/index.js
+++ client/components/Coordinator/EvaluationOverview/General/Controls/index.js
@@ -2,8 +2,9 @@ import Card from 'components/MaterialKit/Card/Card.js';
 import CardBody from 'components/MaterialKit/Card/CardBody.js';
 import Button from 'components/MaterialKit/CustomButtons/Button.js';
 import ManageReviewers from './ManageReviewers.js';
-
 import { useRouter } from 'next/router';
+import { EditEvaluationModal } from 'components/Coordinator/Evaluation/CreateEvaluationModal';
+import { useState } from 'react';
 
 // Store actions and Redux
 import { services } from 'store/feathersClient';
@@ -21,29 +22,39 @@ const setArchived = (evaluation_id, router, value) => {
 
 const Controls = ({ evaluationID, archived }) => {
     const router = useRouter();
-    console.log(archived);
+    const [editModalOpen, setEditModalOpen] = useState(false);
+    const closeModal = () => setEditModalOpen(false);
+    const openModal = () => setEditModalOpen(true);
 
     return (
         <Card>
             <CardBody>
                 {!archived ? (
                     <Button
-                        color="warning"
+                        color="info"
                         onClick={() => makeArchived(evaluationID, router)}
                     >
             Archive
                     </Button>
                 ) : (
                     <Button
-                        color="warning"
+                        color="info"
                         onClick={() => makeUnarchived(evaluationID, router)}
                     >
             Unarchive
                     </Button>
                 )}
 
-                <Button color="warning">Force Completion</Button>
+                <Button color="info">Force Completion</Button>
                 <ManageReviewers evaluationID={evaluationID} />
+                <Button onClick={openModal} color="info">
+          Edit Evaluation
+                </Button>
+                <EditEvaluationModal
+                    closeModal={closeModal}
+                    isOpen={editModalOpen}
+                    evaluationId={evaluationID}
+                />
             </CardBody>
         </Card>
     );
