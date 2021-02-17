2eada08b18bbf93c0922612fbee94ab14df5512b
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-02-17 23:08:50 +0800

    remove dependence of the other information component to user service

--- client/components/Coordinator/EvaluationOverview/General/OtherInformation.js
+++ client/components/Coordinator/EvaluationOverview/General/OtherInformation.js
@@ -2,27 +2,20 @@
 import Card from 'components/MaterialKit/Card/Card.js';
 import CardBody from 'components/MaterialKit/Card/CardBody.js';
 import CardHeader from 'components/MaterialKit/Card/CardHeader.js';
+import Grid from 'components/MaterialKit/Grid/GridContainer.js';
+import GridItem from 'components/MaterialKit/Grid/GridItem.js';
 
 // Store Actions and Redux
 import { useSelector } from 'react-redux';
-import { services } from 'store/feathersClient';
 
-import { useEffect } from 'react';
 
 const OtherInformation = () => {
-    useEffect(() => {
-        services['users'].find();
-    }, []);
-
     const courseEval = useSelector((state) => state['course-evaluation']);
     const evalData = courseEval?.data;
 
-    const users = useSelector((state) => state['users']);
-    const userData = users?.queryResult?.data;
+    const coordinators = evalData?.coordinators  || [];
 
     const createdOn = new Date(evalData?.createdAt);
-    const createdBy = evalData?.createdBy;
-    const author = userData?.find((elem) => elem._id == createdBy);
     const dateString = createdOn?.toLocaleDateString('en-gb', {
         year: 'numeric',
         month: 'short',
@@ -33,11 +26,18 @@ const OtherInformation = () => {
         <Card>
             <CardHeader color="success">Other Information</CardHeader>
             <CardBody>
-                <h4>Created by:</h4>
-                <p>
-                    {author?.name ?? 'Unknown creator'} (on {dateString ?? 'unknown date'}
-          )
-                </p>
+                <Grid direction="row" alignItems="center" justify="center">
+                    <GridItem xs={6}>
+                        <h4>Coordinators</h4>
+                        {coordinators.map(({name},index) => <p key={`coordinators-${index}`}>{name}</p>)}
+                    </GridItem>
+                    <GridItem xs={6}>
+                        <h4>Date Started</h4>
+                        <p>
+                            {dateString ?? 'unknown date'}
+                        </p>
+                    </GridItem>
+                </Grid>
             </CardBody>
         </Card>
     );
