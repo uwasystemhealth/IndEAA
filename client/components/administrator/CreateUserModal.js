d19746c7c1eb754d165328890100183dce3e164b
Michael Nef <michael@nef.net.au>
2021-06-28 10:05:59 +0800

    FIX CreateUserModal import order

--- client/components/administrator/CreateUserModal.js
+++ client/components/administrator/CreateUserModal.js
@@ -1,5 +1,12 @@
+// React + Redux + Functionality
 import React, { useState } from 'react';
-// material-ui components
+import { services } from 'store/feathersClient';
+
+// Material Kit
+import Button from 'components/MaterialKit/CustomButtons/Button.js';
+import CustomInput from 'components/MaterialKit/CustomInput/CustomInput.js';
+
+// Material UI
 import Slide from '@material-ui/core/Slide';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
@@ -8,20 +15,14 @@ import DialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import People from '@material-ui/icons/People';
-// @material-ui/icons
-import Close from '@material-ui/icons/Close';
-// core components
-import Button from 'components/MaterialKit/CustomButtons/Button.js';
-import CustomInput from 'components/MaterialKit/CustomInput/CustomInput.js';
 
-// Redux
-import { services } from 'store/feathersClient';
+// Icons
+import Close from '@material-ui/icons/Close';
 
 // Styles
 import { makeStyles } from '@material-ui/core/styles';
 import modalStyle from 'assets/jss/nextjs-material-kit/modalStyle.js';
 const useStyles = makeStyles(modalStyle);
-
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="down" ref={ref} {...props} />;
