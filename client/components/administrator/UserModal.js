b200ff005bd802dab078226c4af763b7974e53bc
Michael Nef <michael@nef.net.au>
2021-06-28 10:10:47 +0800

    FIX UserModal import order

--- client/components/administrator/UserModal.js
+++ client/components/administrator/UserModal.js
@@ -1,7 +1,24 @@
+// React + Redux + Functionality
 import React, { useState, useEffect } from 'react';
-// material-ui components
+import { useSelector } from 'react-redux';
+import { services } from 'store/feathersClient';
+
+// Custom
 import AreYouSureButton from 'components/Other/AreYouSureButton';
-import DeleteIcon from '@material-ui/icons/Delete';
+import DesignedCheckBox  from 'components/administrator/DesignedCheckBox';
+
+// Utilities
+import {roleIcons} from 'utils/permissions';
+import { getAvailablePermissionsOfUser } from 'utils/permissions';
+
+// Material Kit
+import CustomInput from 'components/MaterialKit/CustomInput/CustomInput.js';
+import Button from 'components/MaterialKit/CustomButtons/Button.js';
+import Grid from 'components/MaterialKit/Grid/GridContainer.js';
+import GridItem from 'components/MaterialKit/Grid/GridItem.js';
+import CustomTabs from 'components/MaterialKit/CustomTabs/CustomTabs.js';
+
+// Material UI
 import Slide from '@material-ui/core/Slide';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
@@ -11,30 +28,15 @@ import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 
-
+// Icons
 import Close from '@material-ui/icons/Close';
-import {roleIcons} from 'utils/permissions';
-
-// core components
-import CustomInput from 'components/MaterialKit/CustomInput/CustomInput.js';
-import Button from 'components/MaterialKit/CustomButtons/Button.js';
-import Grid from 'components/MaterialKit/Grid/GridContainer.js';
-import GridItem from 'components/MaterialKit/Grid/GridItem.js';
-import CustomTabs from 'components/MaterialKit/CustomTabs/CustomTabs.js';
-
-// Utils
-import { getAvailablePermissionsOfUser } from 'utils/permissions';
-
-// Redux
-import { useSelector } from 'react-redux';
-import { services } from 'store/feathersClient';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 // Styles
 import { makeStyles } from '@material-ui/core/styles';
 import checkboxStyles from 'assets/jss/nextjs-material-kit/customCheckboxRadioSwitch.js';
 import modalStyle from 'assets/jss/nextjs-material-kit/modalStyle.js';
 import typographyStyles from 'assets/jss/nextjs-material-kit/pages/componentsSections/typographyStyle.js';
-import DesignedCheckBox  from 'components/administrator/DesignedCheckBox';
 
 export const useStyles = makeStyles({ ...modalStyle, ...typographyStyles, ...checkboxStyles });
 
