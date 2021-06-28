b1b19fdf50990a193ce51bf125a505c26cde136f
Michael Nef <michael@nef.net.au>
2021-06-28 12:01:50 +0800

    FIX assessment import order

--- client/pages/reviewer/[courseID]/3-assessment.js
+++ client/pages/reviewer/[courseID]/3-assessment.js
@@ -1,21 +1,21 @@
+// React + Redux + Functionality
 import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 
-// Use own components// Custom Hooks
-import {useCurrentReviewOfUser} from 'components/customHooks/ReviewerReviewLoad';
+// Utilities
+import { useCurrentReviewOfUser } from 'components/customHooks/ReviewerReviewLoad';
 import useRedirectIfFinish from 'components/customHooks/ReviewerFinishedGuard';
+
+// Custom Components
 import ReviewProgress from 'components/reviewer/ReviewProgress';
 import ReviewerEOCListing from 'components/reviewer/ReviewerEOCListing';
 import ReviewerPageCardDescription from 'components/reviewer/ReviewerPageCardDescription';
 import ReviewerPageBottomNavigation from 'components/reviewer/ReviewerPageBottomNavigation';
 
-// Redux
-import { useSelector } from 'react-redux';
-
-
+// Styles
 import { makeStyles } from '@material-ui/core/styles';
 import styles from 'assets/jss/nextjs-material-kit/pages/landingPage.js';
 const useStyles = makeStyles(styles);
-
 
 const ReviewerCourseReviewPage3 = () => {
     const router = useRouter();
