7ceafcd8ddc8434a56b438e57ed426b9ce87166d
Michael Nef <michael@nef.net.au>
2021-06-28 11:20:46 +0800

    FIX ReviewerDocumentModal import order

--- client/components/reviewer/ReviewerDocumentModal.js
+++ client/components/reviewer/ReviewerDocumentModal.js
@@ -1,30 +1,32 @@
+// React + Redux + Functionality
+import React, { useState, useEffect } from 'react';
+import { services } from 'store/feathersClient';
+
+// Material Kit
 import Button from 'components/MaterialKit/CustomButtons/Button.js';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogActions from '@material-ui/core/DialogActions';
 import GridContainer from 'components/MaterialKit/Grid/GridContainer.js';
 import GridItem from 'components/MaterialKit/Grid/GridItem.js';
 import CustomInput from 'components/MaterialKit/CustomInput/CustomInput.js';
 import Card from 'components/MaterialKit/Card/Card.js';
 import CardBody from 'components/MaterialKit/Card/CardBody.js';
+
+// Material UI
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
-import Close from '@material-ui/icons/Close';
-
-
 import InputAdornment from '@material-ui/core/InputAdornment';
 
-// Redux
-import { services } from 'store/feathersClient';
+// Icons
+import Close from '@material-ui/icons/Close';
 
-// STYLES
+// Styles
 import modalStyle from 'assets/jss/nextjs-material-kit/modalStyle.js';
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
     ...modalStyle,
 });
-
-import React, { useState, useEffect } from 'react';
 
 // COPY PASTED CODE, this should be refactored
 const CustomFormField = ({
