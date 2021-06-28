1a082ffd4bf86a1270c2b8f0c57ccb244bb842fb
Michael Nef <michael@nef.net.au>
2021-06-28 11:15:59 +0800

    FIX Notification import order

--- client/components/Layout/Notification.js
+++ client/components/Layout/Notification.js
@@ -1,20 +1,16 @@
+// React + Redux + Functionality
 import React from 'react';
-// @material-ui/core components
-import { makeStyles } from '@material-ui/core/styles';
-// @material-ui/icons
-import { useSnackbar } from 'notistack';
-// core components
-import Button from 'components/MaterialKit/CustomButtons/Button.js';
-
-// Redux
 import { useSelector, useDispatch } from 'react-redux';
 import { removeNotificationMessage } from 'actions/general';
+import { useSnackbar } from 'notistack';
+
+// Material Kit
+import Button from 'components/MaterialKit/CustomButtons/Button.js';
 
 // Styles
 import styles from 'assets/jss/nextjs-material-kit/pages/componentsSections/notificationsStyles.js';
-
+import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(styles);
-
 
 export default function SectionNotifications() {
     // Built from idea of https://github.com/iamhosseindhv/notistack/issues/116
