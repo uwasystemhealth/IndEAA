aa2339d30e2d64bc3b63264c6bac711edb10389e
Michael Nef <michael@nef.net.au>
2021-06-28 10:24:45 +0800

    FIX EditModal import order

--- client/components/Coordinator/EvaluationOverview/Documents/EditModal.js
+++ client/components/Coordinator/EvaluationOverview/Documents/EditModal.js
@@ -1,38 +1,39 @@
+// React + Redux + Functionality
+import { services } from 'store/feathersClient';
+import React, { useState, useEffect } from 'react';
+
+// Custom Components
+import DesignedCheckBox from 'components/administrator/DesignedCheckBox';
+
+// Utilities
+import { getEOCInfo } from 'utils/eocs';
+
+// Material Kit
 import Button from 'components/MaterialKit/CustomButtons/Button.js';
+import GridContainer from 'components/MaterialKit/Grid/GridContainer.js';
+import GridItem from 'components/MaterialKit/Grid/GridItem.js';
+import CustomInput from 'components/MaterialKit/CustomInput/CustomInput.js';
+import Card from 'components/MaterialKit/Card/Card.js';
+import CardBody from 'components/MaterialKit/Card/CardBody.js';
+import CardHeader from 'components/MaterialKit/Card/CardHeader.js';
+
+// Material UI
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
-import GridContainer from 'components/MaterialKit/Grid/GridContainer.js';
-import GridItem from 'components/MaterialKit/Grid/GridItem.js';
-import CustomInput from 'components/MaterialKit/CustomInput/CustomInput.js';
 import Placeholder from '@material-ui/icons/Subject';
-import Card from 'components/MaterialKit/Card/Card.js';
-import CardBody from 'components/MaterialKit/Card/CardBody.js';
-import CardHeader from 'components/MaterialKit/Card/CardHeader.js';
 import IconButton from '@material-ui/core/IconButton';
 import Close from '@material-ui/icons/Close';
 import List from '@material-ui/core/List';
-
-
 import InputAdornment from '@material-ui/core/InputAdornment';
 
-// Redux
-import { services } from 'store/feathersClient';
-
-// CUSTOM COMPONENTS
-import DesignedCheckBox from 'components/administrator/DesignedCheckBox';
-
-// STYLES
+// Styles
 import modalStyle from 'assets/jss/nextjs-material-kit/modalStyle.js';
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
     ...modalStyle,
 });
-
-import React, { useState, useEffect } from 'react';
-
-import { getEOCInfo } from 'utils/eocs';
 
 const CustomFormField = ({
     label,
