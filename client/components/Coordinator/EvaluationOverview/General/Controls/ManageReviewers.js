c533c535723148c9a9e670637460bee8c04525b2
Michael Nef <michael@nef.net.au>
2021-06-28 10:30:20 +0800

    FIX ManageReviewers import order

--- client/components/Coordinator/EvaluationOverview/General/Controls/ManageReviewers.js
+++ client/components/Coordinator/EvaluationOverview/General/Controls/ManageReviewers.js
@@ -1,35 +1,36 @@
-// CORE COMPONENTS
+// React + Redux + Functionality
+import { useSelector } from 'react-redux';
+import { services, rawServices } from 'store/feathersClient';
+import { useState } from 'react';
+
+// Custom Components
+import ReviewerListing from './ReviewerListing.js';
+
+// Material Kit
+import Button from 'components/MaterialKit/CustomButtons/Button.js';
+import CustomInput from 'components/MaterialKit/CustomInput/CustomInput.js';
+import GridContainer from 'components/MaterialKit/Grid/GridContainer.js';
+import GridItem from 'components/MaterialKit/Grid/GridItem.js';
+
+// Material UI
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
-import Button from 'components/MaterialKit/CustomButtons/Button.js';
-import CustomInput from 'components/MaterialKit/CustomInput/CustomInput.js';
-import SendIcon from '@material-ui/icons/Send';
-import GridContainer from 'components/MaterialKit/Grid/GridContainer.js';
-import GridItem from 'components/MaterialKit/Grid/GridItem.js';
 import IconButton from '@material-ui/core/IconButton';
 import Close from '@material-ui/icons/Close';
 import InputAdornment from '@material-ui/core/InputAdornment';
+
+// Icons
 import People from '@material-ui/icons/People';
+import SendIcon from '@material-ui/icons/Send';
 
-// CUSTOM COMPONENTS
-import ReviewerListing from './ReviewerListing.js';
-
-
-
+// Styles
 import modalStyle from 'assets/jss/nextjs-material-kit/modalStyle.js';
-
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
     ...modalStyle,
 });
-
-// Store Actions and Redux
-import { useSelector } from 'react-redux';
-import { services, rawServices } from 'store/feathersClient';
-
-import { useState } from 'react';
 
 const ManageReviewers = ({ evaluationID }) => {
     const classes = useStyles();
