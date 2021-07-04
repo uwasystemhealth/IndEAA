24bcb378cd6b8d65f0f2675776e6e9fd4096f6b8
Michael Nef <michael@nef.net.au>
2021-07-04 19:02:29 +0800

    spacing changed to 2 spaces (from 4)

--- client/components/reviewer/ReviewListing.js
+++ client/components/reviewer/ReviewListing.js
@@ -11,36 +11,36 @@ import Link from '@material-ui/core/Link';
 //Styles
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(() => ({
-    root: {
-        width: 'inherit',
-    },
+  root: {
+    width: 'inherit',
+  },
 }));
 
 const ReviewListing = ({
-    evalId,
-    review ,
-    courseCode,
-    coordinators,
-    evaluationDescription,
+  evalId,
+  review ,
+  courseCode,
+  coordinators,
+  evaluationDescription,
 }) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const coordinatorNames = coordinators?.map(({name,email})=>name || email).join(', ');
+  const coordinatorNames = coordinators?.map(({name,email})=>name || email).join(', ');
 
-    return (
-        <Grid className={classes.root} direction="row" alignItems="center" justify="center">
-            <GridItem md={7}>
-                <Link href={`/reviewer/${evalId}/1-overview-and-eoc`} style={{textDecoration:'none'}}>
-                    <h3 className={classes.title}>{courseCode}</h3>
-                    <h4 className={classes.description}>{coordinatorNames}</h4>
-                    <h5 className={classes.description}>{evaluationDescription}</h5>
-                </Link>
-            </GridItem>
-            <GridItem md={5}>
-                <ReviewProgress review={review} />
-            </GridItem>
-        </Grid>
-    );
+  return (
+    <Grid className={classes.root} direction="row" alignItems="center" justify="center">
+      <GridItem md={7}>
+        <Link href={`/reviewer/${evalId}/1-overview-and-eoc`} style={{textDecoration:'none'}}>
+          <h3 className={classes.title}>{courseCode}</h3>
+          <h4 className={classes.description}>{coordinatorNames}</h4>
+          <h5 className={classes.description}>{evaluationDescription}</h5>
+        </Link>
+      </GridItem>
+      <GridItem md={5}>
+        <ReviewProgress review={review} />
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default ReviewListing;
