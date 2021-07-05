d67dd3e8d919d1cc1ea53ccb99414d420b7b3039
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-07-05 15:44:47 +0800

    remove local strategy testing

--- server/test/authentication.test.js
+++ server/test/authentication.test.js
@@ -6,19 +6,19 @@ describe('authentication', () => {
     it('registered the authentication service', () => {
         assert.ok(app.service('authentication'));
     });
-    describe('local strategy', () => {
-        const userInfo = userGenerator({ email: 'auth@test.com' });
+    // describe('local strategy', () => {
+    //     const userInfo = userGenerator({ email: 'auth@test.com' });
 
-        before(()=> dropDatabase(app));
-        it('authenticates user and creates accessToken', async () => {
-            await app.service('users').create(userInfo);
+    //     before(()=> dropDatabase(app));
+    //     it('authenticates user and creates accessToken', async () => {
+    //         await app.service('users').create(userInfo);
 
-            const { user, accessToken } = await app.service('authentication').create({
-                strategy: 'local',
-                ...userInfo,
-            });
-            assert.ok(accessToken, 'Created access token for user');
-            assert.ok(user, 'Includes user in authentication data');
-        });
-    });
+    //         const { user, accessToken } = await app.service('authentication').create({
+    //             strategy: 'local',
+    //             ...userInfo,
+    //         });
+    //         assert.ok(accessToken, 'Created access token for user');
+    //         assert.ok(user, 'Includes user in authentication data');
+    //     });
+    // });
 });
