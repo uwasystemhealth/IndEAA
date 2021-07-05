391b7a982e0164b4fc8433b229b77bcac86ca99d
Michael Nef <michael@nef.net.au>
2021-07-05 11:21:51 +0800

    removed unused styles

--- client/components/Coordinator/EvaluationOverview/Justifications/ApplyTo.js
+++ client/components/Coordinator/EvaluationOverview/Justifications/ApplyTo.js
@@ -9,17 +9,7 @@ import CardHeader from 'components/MaterialKit/Card/CardHeader.js';
 // Material UI
 import List from '@material-ui/core/List';
 
-// Styles
-import { makeStyles } from '@material-ui/core/styles';
-import checkboxStyles from 'assets/jss/nextjs-material-kit/customCheckboxRadioSwitch.js';
-const styles = {
-  ...checkboxStyles,
-};
-const useStyles = makeStyles(styles);
-
 const ApplyTo = ({ eocs ,eocInSame,handleCheck}) => {
-  const classes = useStyles();
-
   return (
     <Card>
       <CardHeader color="success">Tags</CardHeader>
