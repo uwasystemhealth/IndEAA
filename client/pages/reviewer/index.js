2c46d5e698f4bb5221b8cb5e83a178359fb8d1aa
Michael Nef <michael@nef.net.au>
2021-07-05 12:13:45 +0800

    reviewer main page removed unused styles

--- client/pages/reviewer/index.js
+++ client/pages/reviewer/index.js
@@ -1,13 +1,7 @@
 // Custom Components
 import ReviewList from 'components/reviewer/ReviewList';
 
-//Styles
-import { makeStyles } from '@material-ui/core/styles';
-import styles from 'assets/jss/nextjs-material-kit/pages/landingPage.js';
-const useStyles = makeStyles(styles);
-
 const ReviewerMainPage = () => {
-  const classes = useStyles();
   return (
     <div>
       <ReviewList />
