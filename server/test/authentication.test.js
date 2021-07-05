38037c1afcbb6d9646e97b9827abe8f38eb14bb9
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-07-05 15:39:43 +0800

    transform server testing to use mocha

--- server/test/authentication.test.js
+++ server/test/authentication.test.js
@@ -1,8 +1,24 @@
+const assert = require('assert');
 const app = require('../src/app');
+const { userGenerator, dropDatabase } = require('./utils');
 
 describe('authentication', () => {
     it('registered the authentication service', () => {
-        expect(app.service('authentication')).toBeTruthy();
+        assert.ok(app.service('authentication'));
     });
-  
+    describe('local strategy', () => {
+        const userInfo = userGenerator({ email: 'auth@test.com' });
+
+        before(()=> dropDatabase(app));
+        it('authenticates user and creates accessToken', async () => {
+            await app.service('users').create(userInfo);
+
+            const { user, accessToken } = await app.service('authentication').create({
+                strategy: 'local',
+                ...userInfo,
+            });
+            assert.ok(accessToken, 'Created access token for user');
+            assert.ok(user, 'Includes user in authentication data');
+        });
+    });
 });
