dd1ad5827727981faab142a10d55c75f39dddc0d
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-07-14 15:57:51 +0800

    fix filepath and more verbose logging

--- server/src/lib/createDocumentFromMarkdown.js
+++ server/src/lib/createDocumentFromMarkdown.js
@@ -5,8 +5,9 @@ const createDocumentFromMarkdown = async (markdownData,filename) => {
     // Use https://pandoc.org/MANUAL.html for args
     // About args format: https://github.com/asaf050/node-pandoc-promise/issues/4
     try{
-        await pandoc(markdownData, `--from markdown -t docx --output ../public/documents/${filename}.docx`.split(' '));
-        console.info('Document Generation is Successful');
+        const filePath = `public/documents/${filename}.docx`;
+        await pandoc(markdownData, `--from markdown -t docx --output ${filePath}`.split(' '));
+        console.info(`Document Generation is Successful: ${filePath}`);
     }
     catch(err){
         console.error(`Failed to convert markdown to document: ${err}`);
