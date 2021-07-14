8a84e02409260d5b7da53fd74702d6d03f583dd9
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-07-14 14:09:36 +0800

    name of file and path to generate

--- server/src/lib/createDocumentFromMarkdown.js
+++ server/src/lib/createDocumentFromMarkdown.js
@@ -1,12 +1,12 @@
 const pandoc = require('node-pandoc-promise');
 
-const createDocumentFromMarkdown = async (markdownData) => {
+const createDocumentFromMarkdown = async (markdownData,filename) => {
     // This function is used for generating word documents with markdown
     // Use https://pandoc.org/MANUAL.html for args
-    // About args: https://github.com/asaf050/node-pandoc-promise/issues/4
+    // About args format: https://github.com/asaf050/node-pandoc-promise/issues/4
     try{
-        await pandoc(markdownData, '--from markdown -t docx --output ./sample.docx'.split(' '));
-        console.info('Success');
+        await pandoc(markdownData, `--from markdown -t docx --output ../public/documents/${filename}.docx`.split(' '));
+        console.info('Document Generation is Successful');
     }
     catch(err){
         console.error(`Failed to convert markdown to document: ${err}`);
