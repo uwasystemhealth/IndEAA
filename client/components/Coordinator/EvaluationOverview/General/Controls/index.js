88c7e134f65100c49b004ee32392610a63a6cc66
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-07-15 17:08:21 +0800

    refactored code to make it straight forward

--- client/components/Coordinator/EvaluationOverview/General/Controls/index.js
+++ client/components/Coordinator/EvaluationOverview/General/Controls/index.js
@@ -12,16 +12,6 @@ import Card from 'components/MaterialKit/Card/Card.js';
 import CardBody from 'components/MaterialKit/Card/CardBody.js';
 import Button from 'components/MaterialKit/CustomButtons/Button.js';
 
-const makeArchived = (evaluation_id, router) =>
-  setArchived(evaluation_id, router, true);
-const makeUnarchived = (evaluation_id, router) =>
-  setArchived(evaluation_id, router, false);
-const setArchived = (evaluation_id, router, value) => {
-  services['course-evaluation'].patch(evaluation_id, {
-    isArchived: value,
-  });
-  router.push('/coordinator');
-};
 
 const Controls = ({ evaluationID, archived }) => {
   const router = useRouter();
@@ -29,20 +19,31 @@ const Controls = ({ evaluationID, archived }) => {
   const closeModal = () => setEditModalOpen(false);
   const openModal = () => setEditModalOpen(true);
 
+  const setArchived = async (evaluation_id, value) => {
+    try{
+      await services['course-evaluation'].patch(evaluation_id, {
+        isArchived: value,
+      });
+      if (value) { router.push('/coordinator'); }
+    }catch(err){
+    //Handled by Redux-Saga
+    }
+  };
+
   return (
     <Card>
       <CardBody>
         {!archived ? (
           <Button
             color="info"
-            onClick={() => makeArchived(evaluationID, router)}
+            onClick={() => setArchived(evaluationID, true)}
           >
             Archive
           </Button>
         ) : (
           <Button
             color="info"
-            onClick={() => makeUnarchived(evaluationID, router)}
+            onClick={() => setArchived(evaluationID, false)}
           >
             Unarchive
           </Button>
