25820344864b0984270f2cb51288eb216a40fad9
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-07-16 11:25:56 +0800

    remove use of embeds
    because it is a pain to deal with

--- client/components/Coordinator/EvaluationOverview/General/index.js
+++ client/components/Coordinator/EvaluationOverview/General/index.js
@@ -15,7 +15,6 @@ import GridItem from 'components/MaterialKit/Grid/GridItem.js';
 import Card from 'components/MaterialKit/Card/Card.js';
 import CardBody from 'components/MaterialKit/Card/CardBody.js';
 import CardHeader from 'components/MaterialKit/Card/CardHeader.js';
-import Button from 'components/MaterialKit/CustomButtons/Button.js';
 
 const General = () => {
   const router = useRouter();
@@ -26,38 +25,19 @@ const General = () => {
   useCurrentCourseData();
 
   const { courseID } = router.query;
-  const reportLink = `${process.env.NEXT_PUBLIC_BACKEND_URL}/documents/${evaluationData?._id}/IndEAA-Report-${evaluationData?.courseId}.docx`;
   return (
     <GridContainer>
-      <GridItem md={6}>
-        <GridContainer>
-          <GridItem xs={12}>
-            <Information />
-          </GridItem>
-          <GridItem xs={12}>
-            <OtherInformation />
-            <Controls
-              evaluationID={courseID}
-              archived={evaluationData?.isArchived}
-            />
-          </GridItem>
-          <GridItem xs={12} ><DocumentsGeneralSection /></GridItem>
-        </GridContainer>
+      <GridItem xs={6}>
+        <Information />
       </GridItem>
-      <GridItem md={6}>
-        <Card style={{ height: '100%' }}>
-          <CardHeader color="success">
-            Course Evaluation in Word {' '}
-            <Button color="rose" external={true} target='_blank' rel="noreferrer" href={reportLink}>
-                Download Document
-            </Button>
-          </CardHeader>
-          <CardBody>
-            <iframe 
-              src={`https://docs.google.com/gview?url=${reportLink}`} width='100%' height='100%' frameBorder='0' />
-          </CardBody>
-        </Card>
+      <GridItem xs={6}>
+        <OtherInformation />
+        <Controls
+          evaluationID={courseID}
+          archived={evaluationData?.isArchived}
+        />
       </GridItem>
+      <GridItem xs={6} ><DocumentsGeneralSection /></GridItem>
     </GridContainer>
   );
 
